f9951216e8e1090cb8cec012c1acd5a326765013
Zijie Jiang <xiaojiang990209@gmail.com>
2017-12-30 20:16:14 -0500

    setup scripts

---
+++ setup.sh
@@ -0,0 +1,30 @@
+# Install git
+sudo apt install git
+
+# Install curl
+sudo apt install curl
+
+# Install zsh
+sudo apt install zsh
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Change default shell
+chsh -s $(which zsh)
+
+# Install Powerline Fonts
+    # clone
+    git clone https://github.com/powerline/fonts.git --depth=1
+    # install
+    cd fonts
+    ./install.sh
+    # clean-up a bit
+    cd ..
+    rm -rf fonts
+
+# Install Powertop
+sudo apt install powertop
+
+# Install tlp
+sudo apt install tlp
